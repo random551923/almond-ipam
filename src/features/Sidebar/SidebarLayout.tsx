af536b3655dfd0418fa6b1380fa3f6ea42f8ef5e
random551923 <random551923@outlook.com>
2026-03-23 08:36:59 +0000

    fix: separate expand/collapse btn to it on component and file

--- src/features/Sidebar/SidebarLayout.tsx
+++ src/features/Sidebar/SidebarLayout.tsx
@@ -1,21 +1,17 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import Logo from "../../components/Logo";
 import { cn } from "../../utils/cn";
 import ThemeSwitch from "./ThemeSwitch";
 import LogoutButton from "./LogoutButton";
 import { useSidebar } from "./SidebarProvider";
 import type React from "react";
+import ExpandButton from "./ExpandButton";
 
 const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
-    const { expanded, toggleSwitch } = useSidebar();
+    const { expanded } = useSidebar();
 
     return (
         <aside className=" relative flex flex-col h-screen bg-sid-bar-bg shadow-md">
-            <button
-                onClick={toggleSwitch}
-                className="p-1.5 absolute top-[70%] -right-3 z-100 rounded-full text-primary-text bg-primary shadow-md hover:bg-primary-light flex items-center justify-center transition-transform transform-gpu hover:scale-110">
-                {expanded ? <ChevronLeft size={15} /> : <ChevronRight size={15} />}
-            </button>
+            <ExpandButton/>
             <div className="m-4 my-6 flex justify-center items-center min-w-max">
                 <Logo expanded={expanded} />
             </div>
