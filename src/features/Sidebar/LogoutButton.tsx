59007d233a5d5cb9228ccbe4ee1357114b5d7268
random551923 <random551923@outlook.com>
2026-03-23 08:29:49 +0000

    fix: center logout button on collapse sidbar

--- src/features/Sidebar/LogoutButton.tsx
+++ src/features/Sidebar/LogoutButton.tsx
@@ -5,14 +5,14 @@ import { useSidebar } from "./SidebarProvider";
 const LogoutButton = () => {
     const { expanded } = useSidebar();
     return (
-        <button className="flex p-2 rounded-md items-center gap-3 text-sid-bar-secondary-text hover:bg-sid-bar-haver-bg">
+        <button className="flex p-2 rounded-md items-center justify-center text-sid-bar-secondary-text hover:bg-sid-bar-haver-bg">
             
-            <span className={cn("overflow-hidden transition-all whitespace-nowrap text-sm",
-                    expanded ? "w-auto opacity-100" : "w-0 opacity-0")}>
+            <span className={cn("overflow-hidden  transition-all whitespace-nowrap text-sm",
+                    expanded ? "w-auto opacity-100 mr-3" : "w-0 opacity-0")}>
                 Logout
             </span>
 
-            <LogOut size={15} className="flex items-center justify-center" />
+            <LogOut size={15} />
 
         </button>
     );
