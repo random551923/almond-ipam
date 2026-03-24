71b6bcd29fac6bdcd6439f6a2f1e714628eb366e
random551923 <random551923@outlook.com>
2026-03-24 13:12:37 +0000

    fix: remove unneseccery class

--- src/features/Sidebar/ExpandButton.tsx
+++ src/features/Sidebar/ExpandButton.tsx
@@ -7,7 +7,7 @@ const ExpandButton = () => {
     return (
         <button
             onClick={toggleSwitch}
-            className="p-1.5 absolute top-[70%] -right-3 z-100 rounded-full text-primary-text bg-primary shadow-md hover:bg-primary-light flex items-center justify-center transition-transform transform-gpu hover:scale-110">
+            className="p-1.5 absolute top-[70%] -right-3 z-100 rounded-full text-primary-text bg-primary shadow-md hover:bg-primary-light flex items-center justify-center transition-transform hover:scale-110">
             {expanded ? <ChevronLeft size={15} /> : <ChevronRight size={15} />}
         </button>
 
