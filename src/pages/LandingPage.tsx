3bb2a7fe5d0cbcba31b0326cd7f650d8c6e89f91
random551923 <random551923@outlook.com>
2026-03-24 14:20:17 +0000

    fix: convert bottom buttons to NavLink

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const LandingPage = () => {
     return (
         <div className="flex-1 flex flex-row items-center justify-center gap-10 ">
@@ -8,12 +10,12 @@ const LandingPage = () => {
                     with ALMOND IPAM easy-to-use, intuitive, centralized console
                 </p>
                 <div className="flex flex-row gap-4 w-full px-10">
-                    <button className="flex-1 px-6 py-2.5 rounded-md whitespace-nowrap bg-container-bg-basic text-primary text-sm font-medium transition-transform hover:scale-105">
+                    <NavLink to="/home" className=" flex flex-1 items-center justify-center px-6 py-2.5 rounded-md whitespace-nowrap bg-container-bg-basic text-primary text-sm font-medium transition-transform hover:scale-105">
                         {"<- Get Started"}
-                    </button>
-                    <button className="px-6 py-2.5 rounded-md whitespace-nowrap text-primary-text border-primary-text border-2 text-sm font-medium transition-transform hover:scale-105">
+                    </NavLink>
+                    <NavLink to="/about" className="flex items-center justify-center px-6 py-2.5 rounded-md whitespace-nowrap text-primary-text border-primary-text border-2 text-sm font-medium transition-transform hover:scale-105">
                         Learn More
-                    </button>
+                    </NavLink>
                 </div>
             </div>
             <img className="w-full w-72 " src={'/src/assets/happy-almond.png'} />
