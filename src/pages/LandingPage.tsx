d13b2dda5ea5b21a64209afefb8b509bcfaddf0f
random551923 <random551923@outlook.com>
2026-03-24 13:41:02 +0000

    feat: Add landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,18 +1,23 @@
 const LandingPage = () => {
     return (
-        <>
-            <h1 className="text-3xl font-bold text-address-free mb-2">
-                Hello Tailwind World!
-            </h1>
-
-            <h1 className="text-3xl font-bold text-primary mb-6">
-                System Theme Text
-            </h1>
-            <h1 className="text-status-info">This text is primary.200</h1>
-            <div>
-                AboutPage
+        <div className="flex-1 flex flex-row items-center justify-center gap-10 ">
+            <div className="flex flex-col gap-10 max-w-lg items-center text-primary-text">
+                <h1 className="text-5xl font-bold text-center">Premium IP Management Solution</h1>
+                <p className="text-center text-lg font-normal ">
+                    Monitor & Manage IP address space, get real time visibility and control
+                    with ALMOND IPAM easy-to-use, intuitive, centralized console
+                </p>
+                <div className="flex flex-row gap-4 w-full px-10">
+                    <button className="flex-1 px-6 py-2.5 rounded-md whitespace-nowrap bg-container-bg-basic text-primary text-sm font-medium transition-transform hover:scale-105">
+                        {"<- Get Started"}
+                    </button>
+                    <button className="px-6 py-2.5 rounded-md whitespace-nowrap text-primary-text border-primary-text border-2 text-sm font-medium transition-transform hover:scale-105">
+                        Learn More
+                    </button>
+                </div>
             </div>
-        </>
+            <img className="w-full w-72 " src={'/src/assets/happy-almond.png'} />
+        </div>
     );
 };
 
