90cec0afbd661c8d4cc39395230550b7862335a1
random551923 <random551923@outlook.com>
2026-03-24 12:14:53 +0000

    fix: fix sidbar main hight and width

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const BaseLayout = () => {
   return (
-      <main className='flex-1 h-fill h-fill min-h-screen px-12 py-10 app-bg overflow-y-auto'>
+      <main className='flex-1 min-h-screen flex flex-col px-12 py-10 app-bg overflow-y-auto'>
         <Outlet />
       </main>
   );
