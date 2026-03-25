5c90d5579fcfcf35325c597f4ff93e3c3d084572
random551923 <random551923@outlook.com>
2026-03-25 10:19:23 +0000

    fix: add shadow to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ import { cn } from "../utils/cn";
 const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
     
     return (
-        <div className={cn("p-8 bg-container-bg-transparent rounded-3xl text-primary-text text-sm", className)}>
+        <div className={cn("p-8 bg-container-bg-transparent rounded-3xl text-primary-text text-sm shadow-md ", className)}>
         {children}
     </div>
   );
