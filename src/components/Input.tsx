d959eabacb7e5df2064e2d458d502448b8273f84
random551923 <random551923@outlook.com>
2026-03-25 17:02:38 +0000

    fix: rebuild Input and login form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,31 +1,26 @@
 
+import { forwardRef } from "react";
 import { cn } from "../utils/cn";
 
 // Extend the input element's interface to include custom props
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    name: string;
-    type?: string;
     label: string;
+    type?: string;
 }
 
-const Input = ({ name, label, type = 'text', className , ...props }: InputProps) => {
- 
-
-    return (
-        <div className="flex flex-col gap-1">
-            <label htmlFor={name} className="text-sm text-secondary-text">{label}</label>
-            <input
-                type={type} id={name} {...props} 
-                className={cn("w-full px-3 py-2 border text-form-primary-text rounded-md shadow-sm focus:outline-none focus:border-primary", className,)}
-            />
-            {/* {error && (
-                <div className='mt-2 text-sm text-status-error'>
-                    {error.message}
-                </div>
-            )} */}
-        </div>
-    );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ label, className, type = "text", ...props }, ref) => {
+        
+        return (
+            <div className="flex flex-col gap-1">
+                <label className="text-sm text-secondary-text">{label}</label>
+                <input type={type} ref={ref}  {...props}
+                    className={cn("w-full px-3 py-2 border text-form-primary-text rounded-md shadow-sm focus:outline-none focus:border-primary", className,)}
+                />
+            </div>
+        );
+    }
+);
 
 export default Input;
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,23 +1,21 @@
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { Button } from "../components/Button";
 import Container from "../components/Container";
 import Input from "../components/Input";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-const logInFormSchema = z.object({
-    userName: z.string()
-        .min(1, { message: 'Please enter a user name' }),
-    password: z.string()
-        .min(1, { message: 'Please enter a password' }),
-});
 
 
-type logInFormSchemaType = z.infer<typeof logInFormSchema>;
+type logInFormSchemaType = {
+    userName: string;
+    password: string;
+}
 
 
 const LoginPage = () => {
-   /*  const form = useForm<logInFormSchemaType>({resolver: zodResolver(logInFormSchema)}); */
+    const {register, handleSubmit, formState:{errors} } = useForm<logInFormSchemaType>();
+
+    const onSubmit: SubmitHandler<logInFormSchemaType> = (data) => {
+        console.log(data);
+    };
 
     return (
         <div className="flex items-center flex-1 justify-center ">
@@ -26,9 +24,10 @@ const LoginPage = () => {
                     <h1 className="text-3xl text-sid-bar-primary-text ">Log In</h1>
                     <p className="text-primary">Welcome back! Please enter your details</p>
                 </div>
-                <form className="flex flex-col w-96 gap-4 py-6">
-                    <Input name='username' label='Username' placeholder='Enter User Name....' />
-                    <Input name='password' label='Password' type="password" placeholder='Enter User Pass....' />
+                <form className="flex flex-col w-96 gap-4 py-6" onSubmit={handleSubmit(onSubmit)}>
+                    <Input  {...register("userName", )} 
+                         label='Username' placeholder='Enter User Name....' />
+                    <Input {...register("password")} label='Password' type="password" placeholder='Enter User Pass....' />
                     <Button type="submit" variant="form" className="w-full hover:bg-primary-light">Submit</Button>
                 </form>
             </Container>
