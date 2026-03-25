f38ef52d13c733d683611b9570a7810f3b636abd
random551923 <random551923@outlook.com>
2026-03-25 08:10:37 +0000

    fix: remove comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter"', 'ui-sans-serif', 'system-ui', 'sans-serif'], /* Default font */
+        sans: ['"Inter"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
         logo: ['"Irish Grover"', 'cursive'],
       },
       colors: {
