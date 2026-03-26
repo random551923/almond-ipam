218b4fc95932d699fb4c2539d887aacf90326b81
random551923 <random551923@outlook.com>
2026-03-26 12:08:28 +0000

    feat: add container title component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,12 +2,22 @@ import type React from "react";
 import { cn } from "../utils/cn";
 
 const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
-    
-    return (
-        <div className={cn("p-8 bg-container-bg-transparent rounded-3xl text-sm shadow-md ", className)}>
-        {children}
+
+  return (
+    <div className={cn("p-8 bg-container-bg-transparent rounded-3xl text-sm shadow-md ", className)}>
+      {children}
     </div>
   );
 };
+
+
+const ContainerTitle = ({ className, text }: { className?: string, text: string }) => {
+
+  return (
+    <h2 className={cn("text-center text-base font-semibold text-primary-text", className)}>{text}</h2>
+  );
+};
+
+Container.title = ContainerTitle;
 
 export default Container;
