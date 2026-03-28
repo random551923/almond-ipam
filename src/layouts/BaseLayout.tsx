a2127156ab4a0e7172308c3399b642daaa228ef9
random551923 <random551923@outlook.com>
2026-03-28 10:24:52 +0000

    fix: fix main layout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const BaseLayout = ({children}:{children?: React.ReactNode}) => {
   return (
-      <main className='flex-1 min-h-screen flex flex-col px-12 py-9 app-bg overflow-y-auto'>
+      <main className='flex-1 min-h-screen flex flex-col px-12 py-9 app-bg'>
          {children ? children : <Outlet />}
       </main>
   );
--- src/layouts/SidebarLayout.tsx
+++ src/layouts/SidebarLayout.tsx
@@ -15,7 +15,7 @@ const SidebarLayout = () => {
           <Sidebar.item icon={<BookOpenText size={20} />} text="About" link="/about" />
         </Sidebar>
       </SidebarProvider>
-      <main className='flex-1 min-h-screen flex flex-col px-12 py-9 app-bg overflow-y-auto'>
+      <main className='flex-1 min-h-screen flex flex-col px-12 py-9 app-bg'>
         <Outlet />
       </main>
     </div>
