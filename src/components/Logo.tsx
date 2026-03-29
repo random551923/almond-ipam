e6f06fa840c8f3f496ca12f18b715292eee6f0a7
random551923@hotmail.com <shakedgoffer@gmail.com>
2026-03-29 01:36:55 -0700

    fix: fix logo size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,21 +4,21 @@ import { cn } from "../utils/cn";
 export default function Logo({ expanded }: { expanded?: boolean; }) {
     return (
         <div className="flex flex-row items-center whitespace-nowrap overflow-hidden ">
-            <LogoIcon size={40} />
+            <LogoIcon  className={'size-10'} />
             <div className={cn("flex flex-col font-logo transition-all duration-500 ease-in-out overflow-hidden",
                 expanded ? " w-auto opacity-100 ml-2 " : "max-w-0 opacity-0 ml-0")} >
-                <span className="text-[23px] leading-none">ALMOND</span>
-                <span className="text-[13px] leading-none ">IP MANAGEMENT</span>
+                <span className="text-2xl leading-none">ALMOND</span>
+                <span className="text-md leading-none ">IP MANAGEMENT</span>
             </div>
         </div>
     );
 }
 
 
-export const LogoIcon = ({ size }: { size: number }) => (
+export const LogoIcon = ({ className }: { className: string }) => (
     <img
         src={'/src/assets/almond-icon.png'}
-        style={{ width: size, height: size }}
+       className={className}
     />
 );
 
@@ -26,7 +26,7 @@ export const LogoIcon = ({ size }: { size: number }) => (
 
 export const LogoFull = () => (
     <div className='flex flex-row items-center font-logo h-fit w-fit'>
-        <LogoIcon size={40} />
+        <LogoIcon className={'siz-40'} />
         <span className='text-4xl'>Almond IPAM</span>
     </div>
 );
