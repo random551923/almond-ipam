1c3c06d77253bb98b3e3925d3d96116d6fe8dc8e
random551923@hotmail.com <shakedgoffer@gmail.com>
2026-03-29 04:48:32 -0700

    fix: them switch width

--- src/features/Sidebar/ThemeSwitch.tsx
+++ src/features/Sidebar/ThemeSwitch.tsx
@@ -20,8 +20,8 @@ const ThemeSwitch = () => {
                 onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
                 className={cn("text-primary h-7 rounded-full bg-slate-200 duration-500  data-[state=checked]:bg-primary",
                     expanded ? "w-14" : " w-10")}>
-                <Switch.Thumb className={cn("flex h-6 w-6 items-center justify-center rounded-full bg-container-bg-basic translate-x-0.5 duration-500 will-change-transform",
-                    expanded ? "data-[state=checked]:translate-x-[30px]" : "data-[state=checked]:translate-x-[14px]")}>
+                <Switch.Thumb className={cn("flex h-6 w-6 items-center justify-center rounded-full bg-container-bg-basic translate-x-[0.2rem] duration-500 will-change-transform",
+                    expanded ? "data-[state=checked]:translate-x-[1.8rem]" : "data-[state=checked]:translate-x-[0.8rem]")}>
                     {theme === "dark" ?
                         (<Moon className="size-4 fill-primary" />) :
                         (<Sun className="size-4 fill-primary" />)
