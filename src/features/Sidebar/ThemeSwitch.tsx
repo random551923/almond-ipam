6283f1b123abb0b8a0258da5305f850a058b6456
random551923@hotmail.com <shakedgoffer@gmail.com>
2026-03-29 04:11:11 -0700

    fix: them switch bug

--- src/features/Sidebar/ThemeSwitch.tsx
+++ src/features/Sidebar/ThemeSwitch.tsx
@@ -12,19 +12,19 @@ const ThemeSwitch = () => {
         <div className="px-1 flex items-center justify-center">
             <span className={cn(" text-sid-bar-secondary-text overflow-hidden transition-all duration-500 whitespace-nowrap text-sm",
                 expanded ? "w-auto opacity-100 mr-3" : "w-0 opacity-0 mr-0")}>
-                {theme === "light" ? "Light" : "Dark"} Mode
+                {theme === "dark" ? "Dark" : "Light"} Mode
             </span>
 
             <Switch.Root
-                checked={theme === "light"}
-                onCheckedChange={(checked) => setTheme(checked ? "light" : "dark")}
+                checked={theme === "dark"}
+                onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
                 className={cn("text-primary h-7 rounded-full bg-slate-200 duration-500  data-[state=checked]:bg-primary",
                     expanded ? "w-14" : " w-10")}>
                 <Switch.Thumb className={cn("flex h-6 w-6 items-center justify-center rounded-full bg-container-bg-basic translate-x-0.5 duration-500 will-change-transform",
                     expanded ? "data-[state=checked]:translate-x-[30px]" : "data-[state=checked]:translate-x-[14px]")}>
-                    {theme === "light" ?
-                        (<Sun className="size-4 fill-primary" />) :
-                        (<Moon className="size-4 fill-primary" />)
+                    {theme === "dark" ?
+                        (<Moon className="size-4 fill-primary" />) :
+                        (<Sun className="size-4 fill-primary" />)
                     }
                 </Switch.Thumb>
             </Switch.Root>
