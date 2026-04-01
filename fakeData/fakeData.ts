900a60404d0c0c6572dfc7329aea4f7646492818
random551923 <random551923@outlook.com>
2026-04-01 08:56:56 +0000

    fix: fix data

--- fakeData/fakeData.ts
+++ fakeData/fakeData.ts
@@ -12,52 +12,52 @@ const data ={
             "20.20.20.10": {
                 "address_description": "test-host1",
                 "mac_address": "00:1B:44:11:3A:B7",
-                "type": "reserved"
+                "type": "dynamic"
             },
             "20.20.20.11": {
-                "address_descriptio": "test-host1",
-                "mac_address_address": "00:1B:44:11:3A:B7",
+                "address_description": "test-host1",
+                "mac_address": "00:1B:44:11:3A:B7",
                 "type": "dynamic"
             },
             "20.20.20.12": {
-                "address_descriptio": "test-host1",
+                "address_description": "test-host1",
                 "mac_address": "00:1B:44:11:3A:B7",
                 "type": "dynamic"
             },
             "20.20.20.13": {
-                "address_descriptio": "test-host1",
+                "address_description": "test-host1",
                 "mac_address": "00:1B:44:11:3A:B7",
                 "type": "dynamic"
             },
             "20.20.20.14": {
-                "address_descriptio": "test-host1",
+                "address_description": "test-host1",
                 "mac_address": "00:1B:44:11:3A:B7",
                 "type": "reserved"
             },
             "20.20.20.40": {
-                "address_descriptio": "test-host1",
+                "address_description": "test-host1",
                 "mac_address": "00:1B:44:11:3A:B7",
                 "type": "reserved"
             },
             "20.20.20.221": {
-                "address_descriptio": "test-host1",
+                "address_description": "test-host1",
                 "mac_address": "00:1B:44:11:3A:B7",
                 "type": "reserved"
             }
         },
         "free_ips": {
             "20.20.20.22": {
-                "address_descriptio": "",
+                "address_description": "",
                 "mac_address": "",
                 "type": "free"
             },
             "20.20.20.23": {
-                "address_descriptio": "",
+                "address_description": "",
                 "mac_address": "",
                 "type": "free"
             },
             "20.20.20.24": {
-                "address_descriptio": "",
+                "address_description": "",
                 "mac_address": "",
                 "type": "free"
             }
@@ -67,7 +67,7 @@ const data ={
     "1.1.1.0": {
         "name": "feddsf",
         "description": "fdgfdg",
-        "geteway": "1.1.1.2",
+        "gateway": "1.1.1.2",
         "dns_servers": [],
         "subnet_cidr": 29,
         "allocated_ips": {
@@ -94,7 +94,7 @@ const data ={
         },
         "free_ips": {
             "1.1.1.1": {
-                "address_descriptio": "",
+                "address_description": "",
                 "mac_address": "",
                 "type": "free"
             }
