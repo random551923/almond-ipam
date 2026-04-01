79e761d25be31aba1222b19a60c1a6c204930c8c
random551923 <random551923@outlook.com>
2026-04-01 07:55:29 +0000

    fix: add props typing

--- src/components/AddressAvailability.tsx
+++ src/components/AddressAvailability.tsx
@@ -1,56 +1,55 @@
 import GuidPieChart from "./charts/GuidPieChart";
 import Container from "./Container";
 
-const AddressAvailability = ({
-  totalFreeAddresses,
-  totalAllocatedAddresses,
-}) => {
-  const total = totalAllocatedAddresses + totalFreeAddresses;
-  const utilization =
-    total > 0 ? Math.round((totalAllocatedAddresses / total) * 100) : 0;
+interface addressAvailabilityProps {
+    totalFreeAddresses: number,
+    totalAllocatedAddresses: number,
+}
+const AddressAvailability = ({totalFreeAddresses, totalAllocatedAddresses,}: addressAvailabilityProps) => {
+    const utilization = Math.round((totalAllocatedAddresses / (totalAllocatedAddresses + totalFreeAddresses)) * 100) : 0;
 
-  const data = [
-    {
-      name: "allocated",
-      value: totalAllocatedAddresses,
-      fill: "var(--color-address-allocated)",
-    },
-    {
-      name: "remaining",
-      value: totalFreeAddresses,
-      fill: "var(--color-address-remaining)",
-    },
-  ];
+    const data = [
+        {
+            name: "allocated",
+            value: totalAllocatedAddresses,
+            fill: "var(--color-address-allocated)",
+        },
+        {
+            name: "remaining",
+            value: totalFreeAddresses,
+            fill: "var(--color-address-remaining)",
+        },
+    ];
 
-  return (
-    <Container className="flex flex-col items-center w-fit">
-      <Container.title text="Address Availability" />
+    return (
+        <Container className="flex flex-col items-center w-fit">
+            <Container.title text="Address Availability" />
 
-      <GuidPieChart data={data} className="size-48 flex flex-1 ">
-        <span className="text-xl font-bold text-primary-text">
-          {utilization}%
-        </span>
-        <span className="text-[10px] uppercase text-secondary-text">Used</span>
-      </GuidPieChart>
+            <GuidPieChart data={data} className="size-48 flex flex-1 ">
+                <span className="text-xl font-bold text-primary-text">
+                    {utilization}%
+                </span>
+                <span className="text-[10px] uppercase text-secondary-text">Used</span>
+            </GuidPieChart>
 
-      <div className="text-primary-text mt-auto mt-4">
-        <ul className="flex flex-1 flex-row justify-around gap-12">
-          {data.map((ipType) => (
-            <li key={ipType.name}>
-              <div className="flex items-center gap-2 text-sm">
-                <div
-                  className="size-[14px] rounded-[3px]"
-                  style={{ backgroundColor: `${ipType.fill}` }}
-                />
-                <span className="capitalize text-lg">{ipType.value}</span>
-              </div>
-              <span className="capitalize text-xs">{ipType.name} </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Container>
-  );
+            <div className="text-primary-text mt-auto mt-4">
+                <ul className="flex flex-1 flex-row justify-around gap-12">
+                    {data.map((ipType) => (
+                        <li key={ipType.name}>
+                            <div className="flex items-center gap-2 text-sm">
+                                <div
+                                    className="size-[14px] rounded-[3px]"
+                                    style={{ backgroundColor: `${ipType.fill}` }}
+                                />
+                                <span className="capitalize text-lg">{ipType.value}</span>
+                            </div>
+                            <span className="capitalize text-xs">{ipType.name} </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </Container>
+    );
 };
 
 export default AddressAvailability;
