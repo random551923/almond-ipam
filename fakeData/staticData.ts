12c25ecc9669a975975a77d3dbf1ef6b6bc89b03
random551923 <random551923@outlook.com>
2026-04-01 09:05:14 +0000

    fix: remove file

--- fakeData/staticData.ts
+++
@@ -1,7 +0,0 @@
-const data = [
-  { name: 'free', value: 5, fill:'var(--color-address-free)',},
-  { name: 'dynamic', value: 10, fill:'var(--color-address-dynamic)',},
-  { name: 'reserved', value: 3, fill:'var(--color-address-reserved)',},
-  
-];
-export default data;
