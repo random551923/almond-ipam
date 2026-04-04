eb04a1bfe7e4e441238eab7af5e49af5d55ff319
random551923 <random551923@outlook.com>
2026-04-04 17:32:00 +0000

    fix: set logout button to navigate to route /

--- src/features/Sidebar/LogoutButton.tsx
+++ src/features/Sidebar/LogoutButton.tsx
@@ -1,11 +1,12 @@
 import { LogOut } from "lucide-react";
 import { cn } from "../../utils/cn";
 import { useSidebar } from "./SidebarProvider";
+import { Link } from "react-router-dom";
 
 const LogoutButton = () => {
     const { expanded } = useSidebar();
     return (
-        <button className="p-1 flex rounded-md items-center justify-center text-sid-bar-secondary-text hover:bg-sid-bar-haver-bg">
+        <Link to="/" className="p-1 flex rounded-md items-center justify-center text-sid-bar-secondary-text hover:bg-sid-bar-haver-bg">
             
             <span className={cn("overflow-hidden transition-all duration-500 whitespace-nowrap text-sm",
                     expanded ? "w-auto opacity-100 mr-3" : "w-0 opacity-0 ml-0")}>
@@ -14,7 +15,7 @@ const LogoutButton = () => {
 
             <LogOut size={18} />
 
-        </button>
+        </Link>
     );
 };
 export default LogoutButton;
